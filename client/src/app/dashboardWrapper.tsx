064db6b3d6879702961d6fdc64ca705b09f42296
amid-hassan <amidhassan589@gamil.com>
2026-09-11 20:02:54 +0530

    added sign in sign up form

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -3,6 +3,7 @@
 import React, { use, useEffect } from 'react'
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
+import AuthProvider from "./authProvider";
 import { Store } from 'lucide-react';
 import StoreProvider, { useAppSelector } from './redux';
 
@@ -36,7 +37,9 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
 const DashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
+      <AuthProvider>
       <DashboardLayout>{children}</DashboardLayout>
+      </AuthProvider>
     </StoreProvider>
   )
 }
