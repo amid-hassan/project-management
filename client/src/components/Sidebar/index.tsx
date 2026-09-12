6ffe92ed9fe4ab4642e5f8df4517833935094ae9
amid-hassan <amidhassan589@gamil.com>
2026-09-12 18:32:54 +0530

    minor changes in Sidebar

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -58,7 +58,7 @@ const Sidebar = () => {
         {/* TOP LOGO */}
         <div className="z-50 flex min-h-[56px] w-64 items-center justify-between bg-white px-6 pt-3 dark:bg-black">
             <div className="text-xl font-bold text-gray-800 dark:text-white">
-                EDLIST
+                ELIST
             </div>
             {isSidebarCollapsed ? null : (
             <button 
@@ -76,7 +76,7 @@ const Sidebar = () => {
             <Image src="https://pm-s3-images-ah.s3.us-east-1.amazonaws.com/logo.png" alt="Logo" width={40} height={40} />
             <div>
                 <h3 className="text-md font-bold tracking-wide dark:text-gray-200">
-            EDROH TEAM
+            EARTH TEAM
                 </h3>
                 <div className="mt-1 flex items-start gap-2">
                     <LockIcon className="mt-[0.1rem] h-3 w-3 text-gray-500 dark:text-gray-400" />
